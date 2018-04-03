99d1cfc7c8b85ee1af5e302fdd28d23a36448f81
Jaydeep Shome <jayshome7@gmail.com>
2018-04-03 20:24:00 +0530

    D22

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,1 +1,2 @@
-run "ln -s #{config.release_path}/backend/vendor #{config.current_path}/vendor"
+#run "ln -s #{config.release_path}/backend/vendor #{config.current_path}/vendor"
+run "rsync -avzh #{config.release_path}/backend/vendor #{config.current_path}/vendor"
