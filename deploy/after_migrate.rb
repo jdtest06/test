2cf7e0e2615675fbf0aad4be9ddfdb981ce83f62
Jaydeep Shome <jayshome7@gmail.com>
2018-04-03 19:04:20 +0530

    D8

--- deploy/after_migrate.rb
+++
@@ -1,1 +0,0 @@
-run "ln -nfs #{config.current_path}/backend/vendor/* #{config.current_path}/vendor/"
---
+++ deploy/before_migrate.rb
@@ -0,0 +1,1 @@
+run "ln -nfs #{config.latest_release}/backend/vendor/* #{config.current_path}/vendor/"
