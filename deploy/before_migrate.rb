5b789f21c771eb36a3e781650de5b24f1311bb79
Jaydeep Shome <jayshome7@gmail.com>
2018-04-03 17:56:51 +0530

    latest

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,2 +1,2 @@
 #run "ln -nfs #{config.shared_path}/public/test #{config.current_path}/public/"
-run "ln -nfs #{config.release_path}/backend/ #{config.current_path}/public"
+run "ln -nfs #{config.release_path}/backend/public/test.html #{config.current_path}/public"
