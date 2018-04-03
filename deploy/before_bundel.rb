85458885557363692eb52071864c7a6eb24f9aab
Jaydeep Shome <jayshome7@gmail.com>
2018-04-03 19:52:03 +0530

    D18

--- deploy/before_bundel.rb
+++
@@ -1,1 +0,0 @@
-run "ln -nfs #{config.release_path}/backend/vendor #{config.current_path}/vendor/"
---
+++ deploy/before_migrate.rb
@@ -0,0 +1,1 @@
+run "ln -nfs #{config.previous_release}/backend/vendor #{config.current_path}/vendor/"
