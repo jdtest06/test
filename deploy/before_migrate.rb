6782ffa254d83d2bf7f63028ff6bb72383794894
Jaydeep Shome <jayshome7@gmail.com>
2018-04-10 01:24:09 +0530

    I0

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -2,4 +2,4 @@
 run "ln -nfs #{config.release_path}/backend/config.ru #{config.release_path}/config.ru"
 run "ln -nfs #{config.release_path}/backend/Gemfile #{config.release_path}/Gemfile"
 run "ln -nfs #{config.release_path}/backend/Gemfile.lock #{config.release_path}/Gemfile.lock"
-run "ln -nfs #{config.release_path}/backend/Rakefile #{config.release_path}/Rakefile"
+#run "ln -nfs #{config.release_path}/backend/Rakefile #{config.release_path}/Rakefile"
