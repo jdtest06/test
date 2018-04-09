ca5eb1a4f7f7043941cb46c256c51d3624d86ece
Jaydeep Shome <jayshome7@gmail.com>
2018-04-10 01:17:09 +0530

    I8

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,2 +1,2 @@
-run "ln -nfs #{config.release_path}/backend/test.html #{config.current_path}/public/test.html"
-run "ln -nfs #{config.release_path}/backend/config.ru #{config.current_path}/config.ru"
+#run "ln -nfs #{config.release_path}/backend/test.html #{config.current_path}/public/test.html"
+run "ln -nfs #{config.release_path}/backend/config.ru #{config.release_path}/config.ru"
