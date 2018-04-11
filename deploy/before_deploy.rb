2de53c2548299088875c7fb3f97010f192973bb8
Daniel Valfre <dvalfre@gmail.com>
2018-04-11 15:37:26 -0300

    clean up deploy hooks

--- deploy/before_deploy.rb
+++ deploy/before_deploy.rb
@@ -1,1 +1,3 @@
+# On 'umbrella' repos, rsync the Rails app's folder to the code's root path
+# so that EY deploy process (serverside) finds it
 run! "rsync -av #{config.release_path}/backend/* #{config.release_path}/"
